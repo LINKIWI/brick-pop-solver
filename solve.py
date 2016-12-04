2d333fa6e27989361c537989bcc1878890b84e58
Kevin Lin <developer@kevinlin.info>
2016-12-03 22:52:29 -0600

    DFS solver

--- solve.py
+++ solve.py
@@ -1,8 +1,8 @@
+import cv2
+
 from board import Board
-from collections import deque
 from color import Color
 from coordinate import Coordinate
-import cv2
 
 
 class SolutionState:
@@ -14,45 +14,41 @@ class SolutionState:
         return '{board}\n{steps}'.format(board=str(self.board), steps=self.steps)
 
 
-def solve_board(board):
-    queue = deque([SolutionState(board, [])])
+def solve_board_dfs(board, steps=[]):
+    if board.is_solved():
+        return steps
 
-    while len(queue) > 0:
-        print len(queue)
-        state = queue.popleft()
-
-        if state.board.is_solved():
-            return state.steps
-
-        for step, new_board in state.board.available_moves():
-            queue.append(SolutionState(new_board, state.steps + [step]))
+    for step, new_board in board.available_moves():
+        solution = solve_board_dfs(new_board, steps + [step])
+        if solution:
+            return solution
 
     return None
 
 
 def replay_steps(board, steps):
-    print steps[0]
-    print board
-    print ''
-
-    if len(steps) == 1:
+    if len(steps) <= 1:
         return
 
-    replay_steps(board.pop_from(steps[0]), steps[1:])
+    new_board = board.pop_from(steps[0])
+    print steps[0]
+    print new_board
+    print ''
+
+    replay_steps(new_board, steps[1:])
 
 
 def load_board():
     img = cv2.imread('brick-pop.png', cv2.IMREAD_COLOR)
 
+    offset = 142
     start_i = 625
-    offset_i = 140
     start_j = 70
-    offset_j = 140
 
     coordinate_map = {}
     for i in range(10):
         for j in range(10):
-            bgr = img[start_i + i * offset_i][start_j + j * offset_j]
+            bgr = img[start_i + i * offset][start_j + j * offset]
             hex = '#%02x%02x%02x' % (bgr[2], bgr[1], bgr[0])
             coordinate_map[Coordinate(i, j)] = Color(hex)
 
@@ -61,8 +57,11 @@ def load_board():
 
 def solve():
     board = load_board()
-    solution = solve_board(board)
+
+    print board
+    solution = solve_board_dfs(board)
     replay_steps(board, solution)
+    print solution
 
 
 if __name__ == '__main__':
